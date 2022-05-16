2b560f3bd3d81e88bf3ffe341c92c9555baafb9d
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2022-05-16 22:42:01 +0100

    Update ModelingInfectiousDiseases.jl

--- src/ModelingInfectiousDiseases.jl
+++ src/ModelingInfectiousDiseases.jl
@@ -3,6 +3,9 @@ module ModelingInfectiousDiseases
 
 using Reexport
 
+# Programmes are grouped by chapter. Each chapter includes a @reexport for each 
+# component programme.
+
 include("chapter2/Chapter2.jl")
 
 end # module ModelingInfectiousDiseases
