04abb50c41fc182a5bc574020d544a3072e5497e
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2022-06-01 10:22:38 +0100

    add programme 3.1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The code is presented in a package. You may install the whole package and use fu
 
 Plots of model outputs are produced within this package in `CairoMakie`. Numerical outputs are available for the user to produce plots using any package of their choice. 
 
-So far, programmes up to the end of chapter 2 (programme 2.7) have been added.
+So far, programmes up to 3.1 in chapter 3 have been added.
 
 Documentation is not yet complete but each function has help text available, which can be accessed with `?` then the function name.
 
--- src/ModelingInfectiousDiseases.jl
+++ src/ModelingInfectiousDiseases.jl
@@ -6,12 +6,16 @@ using DifferentialEquations, Reexport
 @reexport using DifferentialEquations
 
 include("chapter2/Chapter2.jl")
+include("chapter3/Chapter3.jl")
 
 using   # each programme is in its own module  
     # chapter 2:
-    .MID_2_1, .MID_2_2, .MID_2_3, .MID_2_4, .MID_2_5, .MID_2_6, .MID_2_7, .Chapter2Additions 
+    .MID_2_1, .MID_2_2, .MID_2_3, .MID_2_4, .MID_2_5, .MID_2_6, .MID_2_7, .Chapter2Additions,
+    # chapter 3
+    .MID_3_1
 
 @reexport using 
-    .MID_2_1, .MID_2_2, .MID_2_3, .MID_2_4, .MID_2_5, .MID_2_6, .MID_2_7, .Chapter2Additions 
+    .MID_2_1, .MID_2_2, .MID_2_3, .MID_2_4, .MID_2_5, .MID_2_6, .MID_2_7, .Chapter2Additions,
+    .MID_3_1
 
 end # module ModelingInfectiousDiseases
