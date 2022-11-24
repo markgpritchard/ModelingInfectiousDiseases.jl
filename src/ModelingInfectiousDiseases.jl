3f63a97b12ed5a0d2925ea307ae9e66bf6b70882
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2022-11-24 16:57:15 +0000

    Update ModelingInfectiousDiseases.jl

--- src/ModelingInfectiousDiseases.jl
+++ src/ModelingInfectiousDiseases.jl
@@ -2,5 +2,6 @@
 include("chapter2/Chapter2.jl")
 include("chapter3/Chapter3.jl")
 include("chapter4/Chapter4.jl")
+include("chapter4/Chapter5.jl")
 include("chapter6/Chapter6.jl")
 include("chapter7/Chapter7.jl")
